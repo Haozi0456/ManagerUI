8580eda3cb4b5fa1c288f8dc754372dcdc1922a7
Haozi0456 <468164865@qq.com>
2018-08-20 18:09:39 +0800

    提交
    提交

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -18,7 +18,7 @@ fs.open('./build/env.js', 'w', function(err, fd) {
 
 module.exports = merge(webpackBaseConfig, {
     output: {
-        publicPath: 'http://114.115.169.14:8080/ManagerClient/dist/',  // 修改 https://iv...admin 这部分为你的服务器域名
+        publicPath: 'http://114.116.53.54:8080/ManagerClient/dist/',  // 修改 https://iv...admin 这部分为你的服务器域名
         filename: '[name].[hash].js',
         chunkFilename: '[name].[hash].chunk.js'
     },
